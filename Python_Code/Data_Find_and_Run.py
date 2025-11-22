0d2f34a7a194a8119ebb331ac1ab40e2f426ed92
Nelio92 <lionelwandji@gmail.com>
2025-11-22 19:04:32 +0100

    Code updates ongoing...

---
+++ Python_Code/Data_Find_and_Run.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 12:58:54 2025
+
+@author: wandji
+"""
+
+''' The program should do the following:
+    - Open a particular top folder
+    - Search for particular data in all subfolders filtered by a string and data type
+    - Then copy these data in another folder (optionally created by the program)
+    - And load them with a particular software for further data processing
+'''
+
+""" Import of all relevant packages for the job"""
+#import numpy as np
+#import pandas as pd
+# import glob as gb
+# import re
+# from pathlib import Path
+# import os
+# import shutil
+#import matplotlib.pyplot as plt
+#import scipy as sci
+#import lmfit as lm
+#import datetime as dt
+#import time as tm
