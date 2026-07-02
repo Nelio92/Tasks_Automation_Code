02d175eab4c8dc28175115b1e8698d14db3fcc56
Wandji Lionel Wilfried (PSS RF D RAD PTE TE4) <LionelWilfried.Wandji@infineon.com>
2026-07-02 17:34:07 +0200

    feat: Update ODI configuration for Test-D releases with new correlation factors

--- IFX_Scripts/ODI_Transfer_Teslim/ODI_Transfer_Teslim.py
+++ IFX_Scripts/ODI_Transfer_Teslim/ODI_Transfer_Teslim.py
@@ -48,11 +48,13 @@ class OdiImportConfig:
 CONFIG = OdiImportConfig(
 	# Reference workbook (the repo contains CTRX8188A_TE_TX.xlsx)
 	#reference_xlsx=Path(__file__).resolve().parents[2] / "CTRX8188A_TE_TX.xlsx",
-	reference_xlsx=Path(r"C:\UserData\Learning\Software_Programming\GitHub_Nelio92\Tasks_Automation_Code\IFX_Scripts\ODI_Transfer_Teslim\CTRX8188A_TE_TX.xlsx"),
+	#reference_xlsx=Path(r"C:\UserData\Learning\Software_Programming\GitHub_Nelio92\Tasks_Automation_Code\IFX_Scripts\ODI_Transfer_Teslim\CTRX8188A_TE_TX.xlsx"),
+	reference_xlsx=Path(r"C:\UserData\Learning\Software_Programming\GitHub_Nelio92\Tasks_Automation_Code\IFX_Scripts\ODI_Transfer_Teslim\CTRX8144A_TE_TX.xlsx"),
 
 	# Output file to generate
 	#output_csv=Path(__file__).resolve().parents[2] / "ODIs_TX.csv",
-	output_csv=Path(r"C:\UserData\Learning\Software_Programming\GitHub_Nelio92\Tasks_Automation_Code\IFX_Scripts\ODI_Transfer_Teslim\8188_ODIs_TX.csv"),
+	#output_csv=Path(r"C:\UserData\Learning\Software_Programming\GitHub_Nelio92\Tasks_Automation_Code\IFX_Scripts\ODI_Transfer_Teslim\8188_ODIs_TX.csv"),
+	output_csv=Path(r"C:\UserData\Learning\Software_Programming\GitHub_Nelio92\Tasks_Automation_Code\IFX_Scripts\ODI_Transfer_Teslim\8144_ODIs_TX.csv"),
 
 	# Optional filters (examples):
 	# modules={"DPLL", "TXGE"},
@@ -61,15 +63,15 @@ CONFIG = OdiImportConfig(
 	#odi_columns=None,
     #modules={"TXGE","DPLL","TXPA","TXPB","TXPC","TXPD","TXLO","TXPS"},
 	#odi_columns={"ODI LTL S1","ODI UTL S1","ODI LTL S2","ODI UTL S2","ODI LTL B1","ODI UTL B1","ODI LTL B2","ODI UTL B2","ODI LTL Q1","ODI UTL Q1","ODI LTL Q2","ODI UTL Q2","ODI LTL Q3","ODI UTL Q3"},
-	modules={"TXGE"},
+	modules={"TXPA","TXPB","TXPC"},
 	odi_columns={"ODI LTL S1","ODI UTL S1","ODI LTL S2","ODI UTL S2","ODI LTL B1","ODI UTL B1","ODI LTL B2","ODI UTL B2","ODI LTL Q1","ODI UTL Q1","ODI LTL Q2","ODI UTL Q2","ODI LTL Q3","ODI UTL Q3"},
 	# Output metadata
-	comment="Updates related to the Tempco fix for the TXPA stages ICCs",
-	jira_tasks="RSIPPTE-846",
-	#jira_tasks="PTE8144-422",
+	comment="Including the 8188 CV-ATE correlation factors for the Test-D releases",
+	#jira_tasks="RSIPPTE-846",
+	jira_tasks="PTE8144-448",
 	odi_source="INS_OFF",
-	test_variants="8188",
-	#test_variants="8144",
+	test_variants="8144",
+	#test_variants="8188",
 )
 
 
